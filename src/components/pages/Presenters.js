b532739244b5495477b3978df42fa079cd6500df
Barry Rollan <barryrollan@macbook-pro.lan>
2020-10-23 11:16:02 -0400

    added presenters info except for bamby

--- src/components/pages/Presenters.js
+++ src/components/pages/Presenters.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import Menu from '../Menu';
+import Leo from '../../assets/Leo Martinez.jpg';
+import Renato from '../../assets/Renato Perez.png';
+import Mario from '../../assets/Mario Tovar.jpg';
+import Maria from '../../assets/Maria Limon.jpg';
 import { bios } from '../../data/bios';
 import { Card } from 'react-bootstrap';
 import HTMLParser from 'react-html-parser';
@@ -11,10 +15,32 @@ export default function Presenters() {
       <Menu />
       <div className={styles.cardList}>
         {bios.map((b, index) => {
+          let image;
+          switch (true) {
+            case b.imageURL === 'Leo':
+              image = Leo;
+              break;
+            case b.imageURL === 'Renato':
+              image = Renato;
+              break;
+            case b.imageURL === 'Mario':
+              image = Mario;
+              break;
+            case b.imageURL === 'Maria':
+              image = Maria;
+              break;
+            default:
+              console.log(`No image found for ${b.name}`);
+              break;
+          }
           return (
             <Card key={`${b.name}${index}`} className={styles.card}>
               <Card.Header as="h4">{b.name}</Card.Header>
-              <Card.Img variant="top" src={b.imageURL}></Card.Img>
+              <Card.Img
+                variant="top"
+                src={image}
+                className={styles.image}
+              ></Card.Img>
               <Card.Body>{HTMLParser(b.bio)}</Card.Body>
             </Card>
           );
